fb3a6757093db48f652c4f7ea7cfe0b3c56e0767
Henoch0 <44028287+Henoch0@users.noreply.github.com>
2020-09-13 15:20:06 +0200

    11

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,39 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Routes } from '@angular/router';
+import { RunesComponent } from './runes/runes.component';
+import { SettingsComponent } from './settings/settings.component';
+const routes: Routes = [
+  {
+      path: 'runes',
+      component: RunesComponent
+  },
+  {
+      path: 'settings',
+      component: SettingsComponent
+  },
+  {
+    path: 'login',
+    component: SettingsComponent
+},
+{
+  path: 'settings',
+  component: SettingsComponent
+},
+{
+  path: 'settings',
+  component: SettingsComponent
+},
+{
+  path: 'settings',
+  component: SettingsComponent
+},{
+  path: 'settings',
+  component: SettingsComponent
+}
+  
+
+];
 
 @Component({
   selector: 'app-root',
@@ -8,8 +42,4 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent {
   title = 'Runebase';
-  greeting = {};
-  constructor(private http: HttpClient) {
-    http.get('resource').subscribe(data => this.greeting = data);
-  }
 }
